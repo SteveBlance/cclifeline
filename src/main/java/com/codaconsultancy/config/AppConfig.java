3aaa13ef249d6df8e05f1d4dc3223d0494fa1af8
SteveBlance <stephen.blance@gmail.com>
2017-03-19 17:33:15 +0000

    Config changes and initial jsp for app.

--- src/main/java/com/codaconsultancy/config/AppConfig.java
+++ src/main/java/com/codaconsultancy/config/AppConfig.java
@@ -1,21 +1,20 @@
 package com.codaconsultancy.config;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages = {"com.codaconsultancy.controller"})
 public class AppConfig extends WebMvcConfigurerAdapter {
 
-//    @Bean
-//    public ViewResolver jspViewResolver() {
-//        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-//        resolver.setViewClass(JstlView.class);
-//        resolver.setPrefix("/");
-//        resolver.setSuffix(".jsp");
-//        return resolver;
-//    }
+    @Override
+    public void configureDefaultServletHandling(
+            DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+
+
 }
--- src/main/java/com/codaconsultancy/controller/LifelineController.java
+++ src/main/java/com/codaconsultancy/controller/LifelineController.java
@@ -1,23 +1,22 @@
 package com.codaconsultancy.controller;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Map;
 
 @Controller
-@EnableAutoConfiguration
 public class LifelineController {
 
-    @RequestMapping("/")
-    @ResponseBody
-    public String home() {
-        return "home";
-    }
+    // inject via application.properties
+    @Value("${welcome.message:test}")
+    private String message = "Hello World";
 
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(LifelineController.class, args);
+    @RequestMapping("/")
+    public String home(Map<String, Object> model) {
+        model.put("message", this.message);
+        return "index";
     }
 
 }
